5e3c7192d2a5bf5351c95bca62f165a4933e9563
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-04-05 09:10:35 -0500

    Added server-test.js for later testing.

---
+++ server-test.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const PORT = process.env.PORT || 3500;
+
+// app.get('^/$|/index(.html)?', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+// });
+
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
