008073819384bf88b0adf4e51d08bde6f626a51c
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-04-21 20:15:07 -0500

    Updated routes directory per tutorial.

--- INF653VC_Week_12/routes/root.js
+++ INF653VC_Week_12/routes/root.js
@@ -6,13 +6,4 @@ router.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
-router.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'new-page.html'));
-});
-
-router.get('/old-page(.html)?', (req, res) => {
-    // This is a route handler
-    res.redirect(301, '/new-page.html'); // Response code 302 by default. We want 301.
-});
-
 module.exports = router;
